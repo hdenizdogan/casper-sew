ba3a28f9d9d71418c081cbf96f5ba7ff21313f28
mertsariyar <mertsariyar2@gmail.com>
2024-06-28 20:00:08 +0300

    Initial commit

---
+++ cat.js
@@ -0,0 +1,18 @@
+let pingu = document.querySelector(".pingu")
+
+pingu.addEventListener("click", petPingu);
+
+let isPlaying = false;
+
+function petPingu() {
+    if(!isPlaying) {
+        isPlaying = true;
+        let audio = new Audio("voice/pet.mp3")
+        audio.play();
+        audio.volume = 0.3;
+        audio.addEventListener("ended", function() {
+            isPlaying = false;
+        });
+    }
+
+}
